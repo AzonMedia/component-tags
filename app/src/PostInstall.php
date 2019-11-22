315160c438022f6948faeb75f525e58da07c9f6b
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-22 12:38:47 +0200

    fixed hook signature

--- app/src/PostInstall.php
+++ app/src/PostInstall.php
@@ -11,7 +11,7 @@ use GuzabaPlatform\Installer\Interfaces\PostInstallHookInterface;
 
 class PostInstall implements PostInstallHookInterface
 {
-    public static function post_install_hook(Installer $Installer, InstalledRepositoryInterface $Repo, PackageInterface $Package) : bool
+    public static function post_install_hook(Installer $Installer, InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
     {
         //debug
         print 'This is output form the post install hook'.PHP_EOL;
