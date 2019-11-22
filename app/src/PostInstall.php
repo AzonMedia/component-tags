bf260093ace100b51e90c491f32c655c29867218
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-22 12:40:09 +0200

    added update and uninstall hooks

--- app/src/PostInstall.php
+++ app/src/PostInstall.php
@@ -16,4 +16,14 @@ class PostInstall implements PostInstallHookInterface
         //debug
         print 'This is output form the post install hook'.PHP_EOL;
     }
+
+    public static function post_uninstall_hook(Installer $Installer, InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
+    {
+        print 'This is output form the post uninstall hook'.PHP_EOL;
+    }
+
+    public static function post_update_hook(Installer $Installer, InstalledRepositoryInterface $Repo, PackageInterface $InitialPackage, PackageInterface $TargetPackage) : void
+    {
+        print 'This is output form the post update hook'.PHP_EOL;
+    }
 }
