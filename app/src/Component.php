a4f039652d400e26c160f7de9563bb733d26b659
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-22 11:34:49 +0200

    added test install hook

--- app/src/Component.php
+++ app/src/Component.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace GuzabaPlatform\Tags;
 
 use GuzabaPlatform\Components\Base\Interfaces\ComponentInterface;
-use GuzabaPlatform\Components\Base\Interfaces\InstallationHookInterface;
 use GuzabaPlatform\Components\Base\Traits\ComponentTrait;
 
 /**
@@ -19,9 +18,12 @@ class Component implements ComponentInterface
     protected const COMPONENT_NAME = "Tags";
     //https://components.platform.guzaba.org/component/{vendor}/{component}
     protected const COMPONENT_URL = 'https://components.platform.guzaba.org/component/azonmedia/tags';
+    //protected const DEV_COMPONENT_URL//this hsould come from composer.json
     protected const COMPONENT_NAMESPACE = 'GuzabaPlatform\\Tags';
-    protected const COMPONENT_VERSION = 'dev';
+    protected const COMPONENT_VERSION = 'dev';//TODO update this to come from the Composer.json file of the component
     protected const VENDOR_NAME = 'Azonmedia';
     protected const VENDOR_URL = 'https://azonmedia.com';
+
+
 
 }
---
+++ app/src/PostInstall.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace GuzabaPlatform\Tags;
+
+
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use GuzabaPlatform\Installer\Installer;
+use GuzabaPlatform\Installer\Interfaces\PostInstallHookInterface;
+
+class PostInstall implements PostInstallHookInterface
+{
+    public static function post_install_hook(Installer $Installer, InstalledRepositoryInterface $Repo, PackageInterface $Package) : bool
+    {
+        //debug
+        print 'This is output form the post install hook'.PHP_EOL;
+    }
+}
